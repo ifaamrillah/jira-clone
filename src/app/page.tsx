ccfd6444f34d2347307a30f46cb09ae35de4a47a
ifaamrillah <amrillah.work@gmail.com>
2024-10-14 10:12:22 +0700

    feat: modify shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
-      Hello world
-      <Button>Hai</Button>
+    <main className="flex gap-4">
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="tertiary">Tertiary</Button>
     </main>
   );
 }
