d1c0f098d860df3a57afc2406371716b8a179181
ifaamrillah <amrillah.work@gmail.com>
2024-10-14 10:25:45 +0700

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <main className="flex gap-4">
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="tertiary">Tertiary</Button>
+    <main>
+      <Input />
     </main>
   );
 }
